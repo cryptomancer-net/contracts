880fc456db25a15790ff584da51c208d36144826
Alex Klos <alexklos@prohobo.dev>
2022-05-11 18:58:44 +0100

    MVP

---
+++ sources/BOPPaymaster.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: GPT-3
+
+// import "@opengsn/contracts/src/BasePaymaster.sol";
+
+// contract BOPPaymaster is BasePaymaster {
+  
+//   function preRelayedCall(relayRequest, signature, approvalData, maxPossibleGas);
+  
+//   function postRelayedCall(context, success, gasUseWithoutPost, relayData);
+// }
--- sources/Cryptomancer.sol
+++ sources/Cryptomancer.sol
@@ -2,75 +2,92 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: GPT-3
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/utils/cryptography/ECDSA.sol';
+// import "@opengsn/contracts/src/BaseRelayRecipient.sol";
 
 contract Cryptomancer is Ownable {
-    uint8 public feePercentage = 1;
+    using ECDSA for bytes32;
+
+    uint8 public largeFeePercentage = 5;
+    uint8 public smallFeePercentage = 1;
+    address public administrator;
     mapping(address => uint256) public balances;
+    mapping(address => address) public recipientDepositors;
     
-    event Sent(address indexed sender, address indexed recipient, uint256 amount);
+    event Deposited(address indexed depositor, address indexed recipient, uint256 amount, bool paidGasFees);
     event Claimed(address indexed recipient, uint256 amount);
+    event Revoked(address indexed depositor, address indexed recipient, uint256 amount);
     
-    constructor() {}
-    
-    function setFeePercentage(uint8 percent) public onlyOwner {
-        feePercentage = percent;
+    constructor(address _admin) {
+        administrator = _admin;
     }
     
-    function deposit(address[] memory addresses) public payable {
-        uint256 fee = (msg.value * uint256(feePercentage * 100)) / uint256(10000); 
-        payable(owner()).transfer(fee);
-        
-        uint256 amount = msg.value - fee;
-        
-        for (uint256 i = 0; i < addresses.length; i++) {
-            address addr = addresses[i];
-            uint256 recipientAmount = amount / addresses.length;
-            balances[addr] = recipientAmount;
-            emit Sent(msg.sender, addr, recipientAmount);
+    function setFeePercentage(uint8 percent, bool large) public onlyOwner {
+        if (large) {
+            largeFeePercentage = percent;
+        } else {
+            smallFeePercentage = percent;
         }
     }
     
-    function claim(address recipient, bytes32 sig) public {
-        bytes32 message = prefixed(keccak256(abi.encodePacked(recipient, this)));
+    function deposit(address[] memory recipients, bool payGasFees) public payable {
+        uint256 amount = msg.value;
 
-        require(recoverSigner(message, sig) == recipient);
-        require(balances[recipient] > 0);
+        if (payGasFees) {
+            uint256 gasFee = tx.gasprice * gasleft() * recipients.length;
+            require(msg.value >= gasFee + (0.001 ether), 'Minimum value is (gasFee * count(recipients)) + 0.001 ETH');
+            payable(administrator).transfer(gasFee);
+            amount -= gasFee;
+
+            uint256 fee = (amount * uint256(smallFeePercentage * 100)) / uint256(10000); 
+            payable(owner()).transfer(fee);
+            amount -= fee;
+        } else {
+            require(msg.value >= 0.001 ether, 'Minimum value is 0.001 ETH');
+
+            uint256 fee = (amount * uint256(largeFeePercentage * 100)) / uint256(10000); 
+            payable(owner()).transfer(fee);
+            amount -= fee;
+        }
+        
+        for (uint256 i = 0; i < recipients.length; i++) {
+            address recipient = recipients[i];
+            uint256 recipientAmount = amount / recipients.length;
+            if (payGasFees) {
+                balances[recipient] = recipientAmount;
+                recipientDepositors[recipient] = msg.sender;
+            } else {
+                payable(recipient).transfer(recipientAmount);
+            }
+            emit Deposited(msg.sender, recipient, recipientAmount, payGasFees);
+        }
+    }
+    
+    function claim(address recipient, bytes memory signature) public {
+        require(balances[recipient] > 0, 'No balance for recipient');
+
+        bytes32 message = keccak256(abi.encodePacked(recipient));
+        address signer = message.toEthSignedMessageHash().recover(signature);
+
+        require(signer == recipient, 'Signer not the same as recipient');
         
         payable(recipient).transfer(balances[recipient]);
-        balances[recipient] = 0;
         emit Claimed(recipient, balances[recipient]);
+        balances[recipient] = 0;
     }
     
-    function prefixed(bytes32 message) internal pure returns (bytes32) {
-        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", message));
-    }
-    
-    function splitSignature(bytes32 sig) internal pure returns (uint8, bytes32, bytes32) {
-        require(sig.length == 65);
-
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
-
-        assembly {
-            // first 32 bytes, after the length prefix
-            r := mload(add(sig, 32))
-            // second 32 bytes
-            s := mload(add(sig, 64))
-            // final byte (first byte of the next 32 bytes)
-            v := byte(0, mload(add(sig, 96)))
+    function revoke(address[] memory recipients) public {
+        for (uint256 i = 0; i < recipients.length; i++) {
+            address recipient = recipients[i];
+            require(balances[recipient] > 0, 'No deposit for this recipient');
+            require(recipientDepositors[recipient] == msg.sender, 'Not the depositor for this recipient');
+            payable(msg.sender).transfer(balances[recipient]);
+            emit Revoked(msg.sender, recipient, balances[recipient]);
+            balances[recipient] = 0;
         }
-
-        return (v, r, s);
     }
     
-    function recoverSigner(bytes32 message, bytes32 sig) internal pure returns (address) {
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-
-        (v, r, s) = splitSignature(sig);
-
-        return ecrecover(message, v, r, s);
+    function addToGasPool() public payable {
+        payable(administrator).transfer(msg.value);
     }
 }
